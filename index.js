f19628aa038eee1ac45f1a9dfdc43dd2682c437d
Dan Wilhelmsen <dan@explore.org>
2021-09-25 16:33:46 -0700

    Use download url

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ async function run() {
       ref: tags[1].name,
     });
 
-    const currentUrl = currentJson.data.url;
-    const pastUrl = pastJson.data.url;
+    const currentUrl = currentJson.data.download_url;
+    const pastUrl = pastJson.data.download_url;
 
     console.log(`${currentUrl}`);
     console.log(`${pastUrl}`);
