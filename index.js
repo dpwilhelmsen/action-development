899c089691f974efa2c95a0b81594f7c2e18a39b
Dan Wilhelmsen <dan@explore.org>
2021-09-25 16:23:23 -0700

    Load fetch asynchronously

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-import fetch from 'node-fetch';
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 async function run() {
   try {
