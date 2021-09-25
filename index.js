b616ee98a41a1dade04d6272f50b7d23bc229571
Dan Wilhelmsen <dan@explore.org>
2021-09-25 00:49:13 -0700

    Make async function

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const myToken = core.getInput('myToken');
+async function run() {
+  try {
+    const myToken = core.getInput('myToken');
 
-  const octokit = github.getOctokit(myToken)
-  const context = github.context;
-  const tags = await octokit.rest.repos.listTags({
-    ...context.repo,
-  });
+    const octokit = github.getOctokit(myToken)
+    const context = github.context;
+    const tags = await octokit.rest.repos.listTags({
+      ...context.repo,
+    });
 
-  console.log(`tags: ${tags}`);
+    console.log(`tags: ${tags}`);
 
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
